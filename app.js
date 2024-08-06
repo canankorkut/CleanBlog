5c820c92214c81125fb00b70f851d7ada212ebf7
canan <canankorkut1@gmail.com>
2024-08-06 14:41:39 +0300

    Template engine created

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
 
+// Template engine
+app.set("view engine", "ejs");
+
+// Middlewares
+app.use(express.static('public'));
+
+// Routes
 app.get('/', (req, res) => {
-    const blog = { 
-        id:1, 
-        title: "Blog title", 
-        description: "Blog description"
-    };
-    
-    res.send(blog);
-})
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/add', (req, res) => {
+    res.render('add');
+});
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`The server is started on port ${port}.`)
-})
+    console.log(`The server is started on port ${port}.`);
+});
