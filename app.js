8efc68c61cd58c07950d21a8c303afaeb667a089
canan <canankorkut1@gmail.com>
2024-08-13 13:46:05 +0300

    Blog content updated

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const pageController = require('./controllers/pageController');
 const app = express();
 
 // Connect DB
-mongoose.connect('mongodb://127.0.0.1:27017/cleanblog-test-db');
+mongoose.connect('mongodb+srv://canankorkut1:HxqavI5ORBcxww3k@cluster0.qxy2w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
 // Template engine
 app.set("view engine", "ejs");
